8301f321f216584bd80918578b46dd8d1f503ae3
Zach Oda <zjodachow@gmail.com>
2022-06-21 15:52:46 -0400

    type input added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,20 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function (event) {
     event.preventDefault();
-    
-    var listItemEl = document.createElement("li");
+
+var taskNameInput = document.querySelector("input[name='task-name']").value;
+var taskTypeInput = document.querySelector("select[name='task-type']").value;
+//creat list item
+var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
+// create div to hold task info and add to list item
+var taskInfoEl = document.createElement("div");
+//give it a class name
+taskInfoEl.className = "task-info";
+// add HTML content to div
+taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+listItemEl.appendChild(taskInfoEl);
+// add entire list item to list
+tasksToDoEl.appendChild(listItemEl);
 };
 formEl.addEventListener("submit", createTaskHandler); 
